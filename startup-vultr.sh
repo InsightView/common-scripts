226ed07ccf02151fd7e2967b261e1b748343ffb4
Wei <wyhweiyihe@163.com>
2019-07-16 20:07:16 +0800

    Update startup-vultr.sh

--- startup-vultr.sh
+++ startup-vultr.sh
@@ -5,4 +5,8 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 
-docker run --rm -p 10000:8888 -e JUPYTER_ENABLE_LAB=yes -v "$PWD":/home/jovyan/work jupyter/datascience-notebook:9b06df75e445
+systemctl restart docker 
+systemctl daemon-reload
+systemctl enable docker
+
+docker run --rm -p 10000:8888 -e JUPYTER_ENABLE_LAB=yes -v ~:/home/jovyan/work jupyter/minimal-notebook:latest
